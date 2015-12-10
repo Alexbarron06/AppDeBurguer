4772ae02ada96009f5c5d67d9ec56a279b8dd6e4
Alexbarron06 <alexandro.barron@iest.edu.mx>
2015-12-09 21:35:57 -0600

    Initial Commit

---
+++ AppBurger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppBurger
+//
+//  Created by Alexandro Barron  on 09/12/15.
+//  Copyright © 2015 Auto Technology Designs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
